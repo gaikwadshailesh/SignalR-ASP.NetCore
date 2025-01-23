5a2bb11629c6871341866849091101f47e2774e8
gaikwadshailesh <gaikwadshailesh@yahoo.com>
2025-01-23 22:42:14 +0530

    first commit

---
+++ Hubs/ChatHub.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRDemo.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await Clients.All.SendAsync("UserConnected", Context.ConnectionId);
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            await Clients.All.SendAsync("UserDisconnected", Context.ConnectionId);
+            await base.OnDisconnectedAsync(exception);
+        }
+    }
+} 
